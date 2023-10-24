e86a7b11ae87a0243e5a2cca0004f03dc4447fb5
walalaMcfly <129227549+walalaMcfly@users.noreply.github.com>
2023-10-24 00:10:50 -0300

    storage1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor() {
+    //this.grabar_localstorage();
+    this.obtener_localstorage();
+  }
+
+
+  obtener_localstorage(){
+
+    let nombre = localStorage.getItem("nombre");
+    let persona = JSON.stringify(localStorage.getItem("persona"));
+    
+    console.log(nombre);
+    console.log(persona);
+  }
+
+  grabar_localstorage(){
+    let nombre = "Diego";
+    
+    let persona ={
+      nombre:"Tomas",
+      edad: 21,
+      coords:{
+        lat: 10,
+        lng: -10
+
+      }
+
+
+    }
+
+  localStorage.setItem("nombre",nombre);
+  localStorage.setItem("persona",JSON.stringify(persona));
+  }
+
 }
